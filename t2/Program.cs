89a47200271b1b79440e134e40548cef88d3af92
Ivan <off_2020@mail.ru>
2023-02-13 20:05:15 +0500

    t2

---
+++ t2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Количество рандомных чисел: ");
+int val = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[val];
+int max = 0;
+
+ 
+Console.WriteLine();
+ 
+for (int i = 0; i < val; i++)
+{
+      Console.Write("Число {0}: ", i + 1);
+      arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+ 
+
+max = arr[0];
+for (int i = 1; i < val; i++)
+{
+              if (arr[i] > max)
+            max = arr[i];
+}
+ 
+Console.WriteLine(); 
+Console.WriteLine("Max: " + max);
+
+ 
+Console.ReadLine();
